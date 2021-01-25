1589ff858ed6a20187cb16ca18fdad5e46eb286d
hrmorley34 <henry@morley.org.uk>
2021-01-25 20:19:54 +0000

    Improve everything

--- main.py
+++ main.py
@@ -2,17 +2,17 @@
 # -*- encoding: utf-8 -*-
 
 import argparse
-import os
-import sys
+import logging
+from pathlib import Path
 import re
-import glob
+import shutil
 import json
 
 # Set up args
 parser = argparse.ArgumentParser(description="Extracts assets from hashed format")
 parser.add_argument(
     "mcfolder",
-    help="Minecraft data location - must contain assets/indexes and assets/objects",
+    help="Minecraft data location (.minecraft) - must contain assets/indexes and assets/objects",
 )
 parser.add_argument(
     "-t",
@@ -21,24 +21,37 @@ parser.add_argument(
     default="latest",
     help="Version of table to read - defaults to latest",
 )
-parser.add_argument(
+
+outparser = parser.add_mutually_exclusive_group()
+outparser.add_argument(
     "-o",
     "--output",
     dest="loc",
     default="./assets/",
     help="Folder to write files to - defaults to './assets/'",
 )
-parser.add_argument(
+outparser.add_argument(
+    "-l", "--list", dest="list", action="store_true", help="List the files in the table"
+)
+
+filtparser = parser.add_mutually_exclusive_group()
+filtparser.add_argument(
     "-r",
-    "--re",
     "--regex",
     dest="regex",
     default=None,
     help="Regular expression of which files to write",
 )
+filtparser.add_argument(
+    "-g",
+    "--glob",
+    dest="glob",
+    default=None,
+    help="Glob of which files to write (eg. minecraft/**/*.ogg)",
+)
 
-verbosity = parser.add_mutually_exclusive_group()
-verbosity.add_argument(
+verbparser = parser.add_mutually_exclusive_group()
+verbparser.add_argument(
     "-v",
     "--verbose",
     dest="debug",
@@ -46,7 +59,7 @@ verbosity.add_argument(
     default=0,
     help="Increase debug level",
 )
-verbosity.add_argument(
+verbparser.add_argument(
     "-q",
     "--quiet",
     dest="debug",
@@ -56,125 +69,119 @@ verbosity.add_argument(
 )
 
 
-# Functions for debug levels
-def printdebug(level, *text):
-    if DEBUG >= level:
-        print(*text)
-
-
-def printwarn(*text):
-    if DEBUG >= 0:
-        print(*text, file=sys.stderr)
-
-
-# Function to fully expand a path
-def expandpath(path):
-    return os.path.abspath(os.path.expandvars(os.path.expanduser(path)))
+def expandpath(path) -> Path:
+    " Fully expand a path "
+    return Path(path).expanduser().resolve().absolute()
 
 
 if __name__ == "__main__":
     # Parse args
     args = parser.parse_args()
 
-    mcfolder = args.mcfolder
-    tablename = args.table
-    outputfolder = args.loc
-    regexmatch = args.regex
-    DEBUG = args.debug
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=30 - 10 * args.debug)
 
-    mcfolder = expandpath(mcfolder)
-    if not os.path.exists(mcfolder):
-        raise OSError(f"Folder {mcfolder} does not exist")
-    if not os.path.exists(os.path.join(mcfolder, "assets")):
-        raise OSError(f"Folder {mcfolder}/assets does not exist")
-    if not os.path.exists(os.path.join(mcfolder, "assets", "objects")):
-        raise OSError(f"Folder {mcfolder}/assets/objects does not exist")
+    mcfolder = expandpath(args.mcfolder)
+    if not mcfolder.exists():
+        raise FileNotFoundError(f"Folder {mcfolder} does not exist")
+    if not (mcfolder / "assets").exists():
+        raise FileNotFoundError(f"Folder {mcfolder}/assets does not exist")
+    if not (mcfolder / "assets" / "objects").exists():
+        raise FileNotFoundError(f"Folder {mcfolder}/assets/objects does not exist")
+
+    tablename = args.table
 
     if tablename.lower() == "latest":
-        if not os.path.exists(os.path.join(mcfolder, "assets", "indexes")):
-            raise OSError(f"Folder {mcfolder}/assets/indexes does not exist")
-        g = glob.glob(os.path.join(mcfolder, "assets", "indexes", "*.json"))
-        new_g = []
-        for i in g:
+        indexes_folder = mcfolder / "assets" / "indexes"
+        if not indexes_folder.exists():
+            raise FileNotFoundError(f"Folder {indexes_folder} does not exist")
+
+        jsons_by_version = {}
+        for path in indexes_folder.glob("*.json"):
             try:
-                new_g.append(
-                    list(
-                        map(
-                            int,
-                            os.path.abspath(i).split(os.path.sep)[-1].split(".")[:-1],
-                        )
-                    )
-                )
+                version = tuple(map(int, path.stem.split(".")))
+                jsons_by_version[version] = path
             except ValueError:
                 continue
-        try:
-            i = max(new_g)
-        except ValueError:
-            raise OSError("No index tables")
-        i = ".".join(map(str, i))
-        table_json = f"{mcfolder}/assets/indexes/{i}.json"
-    elif os.path.exists(
-        expandpath(os.path.join(mcfolder, "assets/indexes", f"{tablename}.json"))
-    ):
-        table_json = expandpath(
-            os.path.join(mcfolder, "assets/indexes", f"{tablename}.json")
-        )
-    elif os.path.exists(
-        expandpath(os.path.join(mcfolder, "assets", "indexes", tablename))
-    ):
-        table_json = expandpath(os.path.join(mcfolder, "assets", "indexes", tablename))
-    elif os.path.exists(expandpath(tablename)):
+
+        if len(jsons_by_version) == 0:
+            raise FileNotFoundError(f"No index tables found in {indexes_folder}")
+        else:
+            maxversion = max(jsons_by_version)
+            table_json = jsons_by_version[maxversion]
+
+    elif (mcfolder / "assets" / "indexes" / f"{tablename}.json").exists():
+        table_json = mcfolder / "assets" / "indexes" / f"{tablename}.json"
+
+    elif (mcfolder / "assets" / "indexes" / tablename).exists():
+        table_json = mcfolder / "assets" / "indexes" / tablename
+
+    elif expandpath(tablename).exists():
         table_json = expandpath(tablename)
+
     else:
-        raise OSError("No index table")
-    printdebug(1, f"JSON file address generated: {table_json}")
+        raise FileNotFoundError("No index table")
 
-    outputfolder = expandpath(outputfolder)
-    if not os.path.exists(outputfolder):
-        printwarn(f"{outputfolder} not found - creating")
-        os.makedirs(outputfolder)
+    logging.info(f"JSON file found: {table_json}")
 
-    if regexmatch is not None:
-        try:
-            regexmatch = re.compile(regexmatch)
-            printdebug(1, f"Compiled regex: {regexmatch}")
-        except re.error as reerror:
-            printwarn(f"Invalid regex {regexmatch}: {reerror}")
-            regexmatch = None
+    if not args.list:
+        outputfolder = expandpath(args.loc)
+        if not outputfolder.exists():
+            logging.warning(f"{outputfolder} not found - creating")
+            outputfolder.mkdir(parents=True, exist_ok=True)
 
     with open(table_json, mode="r") as f:
-        try:
-            json_data = json.load(f)
-        except json.decoder.JSONDecodeError:
-            raise
-        printdebug(1, f"Read json from {table_json}")
+        json_data = json.load(f)
+        logging.info(f"Read json from {table_json}")
 
-    printdebug(1, f"Starting write: {len(json_data['objects'])} items (non-filtered)")
-    for k, v in json_data["objects"].items():
-        if regexmatch is None or regexmatch.fullmatch(k):
-            printdebug(2, f"Matched {k}")
-            h = v["hash"]
-            printdebug(2, f"Reading hash {h}")
-            if os.path.exists(os.path.join(mcfolder, "assets/objects", h[:2], h)):
-                with open(
-                    os.path.join(mcfolder, "assets/objects", h[:2], h), mode="rb"
-                ) as f:
-                    data = f.read()
-                printdebug(3, "Read data")
-                if not os.path.exists(os.path.join(outputfolder, os.path.split(k)[0])):
-                    os.makedirs(os.path.join(outputfolder, os.path.split(k)[0]))
-                if os.path.exists(os.path.join(outputfolder, k)):
-                    printwarn(
-                        f"{os.path.join(outputfolder, k)} already exists - overwriting"
-                    )
-                with open(os.path.join(outputfolder, k), mode="wb") as f:
-                    f.write(data)
-                printdebug(3, "Wrote data")
+    if args.regex is not None:
+        regexmatch = re.compile(args.regex)
+        logging.info(f"Compiled regex: {regexmatch}")
+
+        jobjects = {}
+        for k in json_data["objects"]:
+            if regexmatch.match(k):
+                logging.debug(f"Matched {k}")
+                jobjects[k] = json_data["objects"][k]
             else:
-                printwarn(f"Can't find hash {h}")
-        else:
-            printdebug(3, f"Failed to match {k}")
-    printdebug(1, "Finished write")
+                logging.debug(f"Failed to match {k}")
+                continue
+    elif args.glob is not None:
+        jobjects = {}
+        for k in json_data["objects"]:
+            if Path(k).match(args.glob):
+                logging.debug(f"Matched {k}")
+                jobjects[k] = json_data["objects"][k]
+            else:
+                logging.debug(f"Failed to match {k}")
+                continue
+    else:
+        jobjects = json_data["objects"]
 
-    printdebug(0, "Finished.")
-    sys.exit()
+    if len(jobjects) == 0:
+        logging.warning("Zero items match the filter.")
+    elif args.list:
+        logging.info(f"Listing {len(jobjects)} items")
+        for k, v in sorted(jobjects.items()):
+            print("{}: {}".format(v["hash"], k))
+    else:
+        logging.info(f"Starting: {len(jobjects)} items")
+        for k, v in jobjects.items():
+            h = v["hash"]
+
+            logging.debug(f"Finding hash {h}")
+            hfile = mcfolder / "assets" / "objects" / h[:2] / h
+            if hfile.exists():
+                ofile = outputfolder / k
+                ofile.parents[0].mkdir(parents=True, exist_ok=True)
+                if ofile.exists():
+                    logging.warning(f"{ofile} already exists - overwriting")
+
+                logging.debug(f"Copying file {hfile}...")
+                shutil.copy2(hfile, ofile)
+                logging.debug("Copied file")
+            else:
+                logging.warning(f"Can't find hash {h} (at {hfile})")
+
+        logging.info("Finished.")
+
+    exit(0)
