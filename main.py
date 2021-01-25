058e29e9f0b9c5f295bdc597bf3c84520ee1ff4a
hrmorley34 <henry@morley.org.uk>
2021-01-25 17:30:57 +0000

    Blacken and other fixes

--- main.py
+++ main.py
@@ -2,39 +2,75 @@
 # -*- encoding: utf-8 -*-
 
 import argparse
-import os, sys
+import os
+import sys
 import re
 import glob
 import json
 
 # Set up args
 parser = argparse.ArgumentParser(description="Extracts assets from hashed format")
-parser.add_argument("mcfolder",
-                    help="Minecraft data location - must contain assets/indexes and assets/objects")
-parser.add_argument("-t", "--table", dest="table", default="latest",
-                    help="Version of table to read - defaults to latest")
-parser.add_argument("-o", "--output", dest="loc", default="./assets/",
-                    help="Folder to write files to - defaults to './assets/'")
-parser.add_argument("-r", "--re", "--regex", dest="regex", default=None,
-                    help="Regular expression of which files to write")
+parser.add_argument(
+    "mcfolder",
+    help="Minecraft data location - must contain assets/indexes and assets/objects",
+)
+parser.add_argument(
+    "-t",
+    "--table",
+    dest="table",
+    default="latest",
+    help="Version of table to read - defaults to latest",
+)
+parser.add_argument(
+    "-o",
+    "--output",
+    dest="loc",
+    default="./assets/",
+    help="Folder to write files to - defaults to './assets/'",
+)
+parser.add_argument(
+    "-r",
+    "--re",
+    "--regex",
+    dest="regex",
+    default=None,
+    help="Regular expression of which files to write",
+)
 
 verbosity = parser.add_mutually_exclusive_group()
-verbosity.add_argument("-v", "--verbose", dest="debug", action="count",
-                       default=0, help="Increase debug level")
-verbosity.add_argument("-q", "--quiet", dest="debug", action="store_const",
-                       const=-1, help="Only output errors, not warnings")
+verbosity.add_argument(
+    "-v",
+    "--verbose",
+    dest="debug",
+    action="count",
+    default=0,
+    help="Increase debug level",
+)
+verbosity.add_argument(
+    "-q",
+    "--quiet",
+    dest="debug",
+    action="store_const",
+    const=-1,
+    help="Only output errors, not warnings",
+)
+
 
 # Functions for debug levels
 def printdebug(level, *text):
     if DEBUG >= level:
         print(*text)
+
+
 def printwarn(*text):
     if DEBUG >= 0:
         print(*text, file=sys.stderr)
 
+
 # Function to fully expand a path
 def expandpath(path):
-    return(os.path.abspath(os.path.expandvars(os.path.expanduser(path))))
+    return os.path.abspath(os.path.expandvars(os.path.expanduser(path)))
+
 
 if __name__ == "__main__":
     # Parse args
@@ -60,17 +96,32 @@ if __name__ == "__main__":
         g = glob.glob(os.path.join(mcfolder, "assets", "indexes", "*.json"))
         new_g = []
         for i in g:
-            try: new_g.append(list(map(int, os.path.abspath(i).split(os.path.sep)[-1].split(".")[:-1])))
-            except ValueError: continue
+            try:
+                new_g.append(
+                    list(
+                        map(
+                            int,
+                            os.path.abspath(i).split(os.path.sep)[-1].split(".")[:-1],
+                        )
+                    )
+                )
+            except ValueError:
+                continue
         try:
             i = max(new_g)
         except ValueError:
             raise OSError("No index tables")
         i = ".".join(map(str, i))
         table_json = f"{mcfolder}/assets/indexes/{i}.json"
-    elif os.path.exists(expandpath(os.path.join(mcfolder, "assets/indexes", f"{tablename}.json"))):
-        table_json = expandpath(os.path.join(mcfolder, "assets/indexes", f"{tablename}.json"))
-    elif os.path.exists(expandpath(os.path.join(mcfolder, "assets", "indexes", tablename))):
+    elif os.path.exists(
+        expandpath(os.path.join(mcfolder, "assets/indexes", f"{tablename}.json"))
+    ):
+        table_json = expandpath(
+            os.path.join(mcfolder, "assets/indexes", f"{tablename}.json")
+        )
+    elif os.path.exists(
+        expandpath(os.path.join(mcfolder, "assets", "indexes", tablename))
+    ):
         table_json = expandpath(os.path.join(mcfolder, "assets", "indexes", tablename))
     elif os.path.exists(expandpath(tablename)):
         table_json = expandpath(tablename)
@@ -94,7 +145,7 @@ if __name__ == "__main__":
     with open(table_json, mode="r") as f:
         try:
             json_data = json.load(f)
-        except json.decoder.JSONDecodeError as jsonerror:
+        except json.decoder.JSONDecodeError:
             raise
         printdebug(1, f"Read json from {table_json}")
 
@@ -105,16 +156,20 @@ if __name__ == "__main__":
             h = v["hash"]
             printdebug(2, f"Reading hash {h}")
             if os.path.exists(os.path.join(mcfolder, "assets/objects", h[:2], h)):
-                with open(os.path.join(mcfolder, "assets/objects", h[:2], h), mode="rb") as f:
+                with open(
+                    os.path.join(mcfolder, "assets/objects", h[:2], h), mode="rb"
+                ) as f:
                     data = f.read()
-                printdebug(3, f"Read data")
+                printdebug(3, "Read data")
                 if not os.path.exists(os.path.join(outputfolder, os.path.split(k)[0])):
                     os.makedirs(os.path.join(outputfolder, os.path.split(k)[0]))
                 if os.path.exists(os.path.join(outputfolder, k)):
-                    printwarn(f"{os.path.join(outputfolder, k)} already exists - overwriting")
+                    printwarn(
+                        f"{os.path.join(outputfolder, k)} already exists - overwriting"
+                    )
                 with open(os.path.join(outputfolder, k), mode="wb") as f:
                     f.write(data)
-                printdebug(3, f"Wrote data")
+                printdebug(3, "Wrote data")
             else:
                 printwarn(f"Can't find hash {h}")
         else:
